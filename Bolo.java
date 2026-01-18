0479c59765a1e1fcbf86d6e31a882e2c8bea2ab6
Isabella Costa <166655665+isabellatsco@users.noreply.github.com>
2026-01-18 19:23:29 -0300

    + classe Bolo

--- Bolo.java
+++ Bolo.java
@@ -1,5 +1,28 @@
 package soBolos;
 
-public class Bolo {
+public abstract class Bolo implements IBolo {
+	private int codigo;
+	private double custo;
+	private IFormato formato;
+	
+	public Bolo(int codigo, double custo) {
+		this.setCodigo(codigo);
+		this.setCusto(custo);
+	}
+	
+	public Bolo(int codigo, double custo, IFormato formato) {
+        this.setCodigo(codigo);
+        this.setCusto(custo);
+        this.setFormato(formato);
+    }
+	
+	public int getCodigo() { return codigo; }
+    public void setCodigo(int codigo) { this.codigo = codigo; }
+
+    public double getCusto() { return custo; }
+    public void setCusto(double custo) { this.custo = custo; }
+
+    public IFormato getFormato() { return formato; }
+    public void setFormato(IFormato formato) { this.formato = formato; }
 
 }
